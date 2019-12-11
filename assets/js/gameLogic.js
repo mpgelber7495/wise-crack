09af489ddc0e79c0214c9af468dd23048c699bcc
Paul Ballas <paul.ballas@richmond.edu>
2019-12-11 14:17:42 -0500

    feature: add bootstrap cdn and jquery cdn, add two buttons to home screen. Add game ID generator

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -19,3 +19,10 @@ function listenToData(collection, doc, functionToExecute) {
       functionToExecute;
     });
 }
+
+// generates random number ID
+var gameID = Math.random()
+  .toString(36)
+  .substr(2, 9);
+
+console.log(gameID);
