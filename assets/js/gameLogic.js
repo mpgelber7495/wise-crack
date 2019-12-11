516688df938b7b43f9a81de6e0a5f95ae3820289
Paul Ballas <paul.ballas@richmond.edu>
2019-12-11 14:55:26 -0500

    feature: add buttons while keeping HTML clean

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -20,6 +20,22 @@ function listenToData(collection, doc, functionToExecute) {
     });
 }
 
+// create game and start game button
+
+$(".container")[0].innerHTML += `
+<div class="col-12 mb-4 create-row d-flex justify-content-center">
+  <button type="button" class="btn btn-secondary btn-lg create-game-btn">
+    Create New Game
+  </button>
+</div>
+
+
+<div class="col-12 join-row d-flex justify-content-center">
+  <button type="button" class="btn btn-secondary btn-lg join-game-btn">
+    Join Existing Game
+  </button>
+</div>`;
+
 // generates random number ID
 var gameID = Math.random()
   .toString(36)
