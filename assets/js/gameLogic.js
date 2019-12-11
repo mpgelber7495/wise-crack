f1d27ef320c23bc3d894e0285a4ff522717870ed
Michael Gelber <michael.gelber@xandr.com>
2019-12-11 18:35:18 +0000

    Feature: built countDown function

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -31,11 +31,22 @@ function listenToData(collection, doc, functionToExecute) {
 // TO-DO: Function for running game as judge
 // ------------------------------------------------
 
-//Function for setting the prompt in the database
+// Function for setting the prompt in the database
 function setPrompt(gameID, roundID, prompt) {
   db.collection(gameID)
     .doc(roundID)
     .set({ prompt: prompt });
+}
+
+// Function for counting down from 40 seconds
+function countDown() {
+  let timeHolder = 40;
+  var counter = setInterval(function() {
+    timeHolder--;
+    if (timeHolder < 0) {
+      clearInterval(counter);
+    }
+  }, 1000);
 }
 
 function runRoundAsJudge(gameID, roundID) {
