5dfc3072a964261edde1b436d607fca9dcf0811a
Michael Gelber <mpgelber7495@gmail.com>
2019-12-11 22:01:16 -0500

    Update: to meet baulpallas's PR

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -42,3 +42,35 @@ var gameID = Math.random()
   .substr(2, 9);
 
 console.log(gameID);
+
+// ------------------------------------------------
+// TO-DO: Function for running game as judge
+// ------------------------------------------------
+
+// Function for setting the prompt in the database
+function setPrompt(gameID, roundID, prompt) {
+  db.collection(gameID)
+    .doc(roundID)
+    .set({ prompt: prompt });
+}
+
+// Function for counting down from 40 seconds
+function countDown(gameID) {
+  let timeHolder = 40;
+  var counter = setInterval(function() {
+    timeHolder--;
+    writeDataMerge(gameID, "logistics", { timeHolder: timeHolder });
+    if (timeHolder < 1) {
+      clearInterval(counter);
+    }
+  }, 1000);
+}
+
+function runRoundAsJudge(gameID, roundID) {
+  countDown(gameID);
+  // setPrompt();
+
+  //
+}
+
+runRoundAsJudge("gameXYZ", "round3");
