b8c8d7ba14469077ad1dd9e9a761cb3cb729b37e
Vaniri <foxifennec@gmail.com>
2019-12-12 19:58:57 -0500

    Feat: added player function

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -42,3 +42,36 @@ var gameID = Math.random()
   .substr(2, 9);
 
 console.log(gameID);
+
+//player function
+
+const collectiondRef = db.collection("Game123");
+
+function player(nickname, time, question) {
+    const gameContainer = $("#container");
+    gameContainer.append(question);
+    gameContainer.append("<br>");
+    const labelAnswer = $('<label for="answer-input">Enter your answer!</label> ');
+    const playerAnswer = $('<input id="answer-input" type="text"/>');
+    const submitAnswer = $('<input type="button" id="submit" value="Submit answer!"/>');
+    const timer = $('<h1 id="timer"></h1>');
+    timer.text(`You have ${time} seconds left`);
+    gameContainer.prepend(timer);
+    gameContainer.append(labelAnswer);
+    gameContainer.append(playerAnswer);
+    gameContainer.append(submitAnswer);
+    submitAnswer.on("click", () => {
+        event.preventDefault();
+        let answer = playerAnswer.val();
+        if(answer === "") { gameContainer.text("No answer"); }
+        //send answer to the firestore
+        ollectiondRef.doc(nickname).set({
+            nickname: answer,
+        })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
+        gameContainer.text("We got your answer!");
+    })
+}
+
