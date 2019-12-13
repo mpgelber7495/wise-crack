b9c456c92b814af5d29f0fd8f824ea15e974bf02
Michael Gelber <mpgelber7495@gmail.com>
2019-12-12 21:03:31 -0500

    Feature: Round Instantiation function

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -43,12 +43,36 @@ $(".container")[0].innerHTML += `
 // console.log(gameID);
 
 // ------------------------------------------------
+// TO-DO: Instantiate Round
+// ------------------------------------------------
+function instantiateRound() {
+  db.collection(gameID)
+    .doc("logistics")
+    .get()
+    .then(function(doc) {
+      let judge = doc.data()["judge"];
+      if (judge === nickname) {
+        let roundCount = doc.data()["roundCounter"] + 1;
+        let newRoundID = "round" + roundCount;
+        let data = {};
+        data["winningPlayer"] = "null";
+        db.collection(gameID)
+          .doc(newRoundID)
+          .set(data);
+        runRoundAsJudge(newRoundID);
+      } else {
+        runGameAsPlayer(nickname, newRoundID);
+      }
+    });
+}
+
+// ------------------------------------------------
 // TO-DO: Function for player
 // ------------------------------------------------
 
 const collectiondRef = db.collection("Game123");
 
-function player(nickname, time) {
+function runGameAsPlayer(nickname, roundID) {
   const gameContainer = $(".container");
   let prompt = "";
   db.collection(gameID)
@@ -69,10 +93,10 @@ function player(nickname, time) {
   db.collection(gameID)
     .doc("logistics")
     .onSnapshot(function(doc) {
-      time = doc.data()["timeHolder"];
+      var time = doc.data()["timeHolder"];
+      timer.text(`You have ${time} seconds left`);
+      gameContainer.prepend(timer);
     });
-  timer.text(`You have ${time} seconds left`);
-  gameContainer.prepend(timer);
   gameContainer.append(labelAnswer);
   gameContainer.append(playerAnswer);
   gameContainer.append(submitAnswer);
@@ -83,31 +107,19 @@ function player(nickname, time) {
       gameContainer.text("No answer");
     }
     //send answer to the firestore
-    ollectiondRef
-      .doc(nickname)
-      .set({
-        nickname: answer
-      })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
-      });
+    let data = {};
+    data[nickname] = answer;
+    writeDataMerge(gameID, roundID, data);
     gameContainer.text("We got your answer!");
   });
 }
 
-//API using Card cast, find a deck code and input below https://www.cardcastgame.com/browse?nsfw=1
-
-var deckId = "8BQAD";
-
-var queryURL = "https://api.cardcastgame.com/v1/decks/" + deckId + "/cards";
-
 // ------------------------------------------------
 // TO-DO: Function for running game as judge
 // ------------------------------------------------
-
 // Using stagnant gameID for development
-const gameID = "Game123";
 
+let gameID = "Game123";
 // Setting an array equal to the players who have signed up via path gameID >> Logistics >> players
 let playersArray = [];
 db.collection(gameID)
@@ -121,7 +133,12 @@ function runRoundAsJudge(roundID) {
   setRandomPrompt(roundID);
 }
 // Function for setting the prompt in the database
+
 function setRandomPrompt(roundID) {
+  // API using Card cast, find a deck code and input below https://www.cardcastgame.com/browse?nsfw=1
+  var deckId = "8BQAD";
+  var queryURL = "https://api.cardcastgame.com/v1/decks/" + deckId + "/cards";
+
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -213,4 +230,5 @@ function changeJudge(newJudge) {
   writeDataMerge(gameID, "logistics", judgeData);
 }
 
-runRoundAsJudge("round1");
+let nickname = "Ivo";
+instantiateRound();
